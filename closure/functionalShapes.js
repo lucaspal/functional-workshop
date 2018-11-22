6ceb31a33dca5d6ff2b7664fd42aec1b6c79f87c
Luca Spalierno <spalierno@gmail.com>
2018-11-22 15:09:03 +0100

    Add closure exercise.

--- closure/functionalShapes.js
+++ closure/functionalShapes.js
@@ -4,10 +4,10 @@ const rectangle = (height, width) => {
   return (method) => {
     switch (method) {
       case 'getWidth': return () => width;
-      case 'getHeight': return  //TODO
-      case 'getArea': return //TODO
+      case 'getHeight': return () => height;
+      case 'getArea': return () => height * width;
       case 'setWidth': return (newWidth) => rectangle(height, newWidth);
-      case 'setHeight': return  //TODO
+      case 'setHeight': return  (newHeigth) => rectangle(newHeigth, width);
       case 'toString': return () => new Array(height+1).join(new Array(width+1).join('X ') + '\n');
       default: return (...args) => `unknown method "${method}"`;
     }
@@ -17,9 +17,9 @@ const rectangle = (height, width) => {
 const square = (side) => {
   return (method) => {
     if (method === 'setSize' || method === 'setWidth' || method === 'setHeight') {
-      return (size) => // TODO
+      return (size) => square(size);
     }
-    else return //TODO
+    else return rectangle(side, side)(method);
   }
 };
 
