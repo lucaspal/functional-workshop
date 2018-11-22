1a1d951309b8a8f21226e5e2c5f61eb81f21ecf3
Luca Spalierno <spalierno@gmail.com>
2018-11-22 15:47:09 +0100

    Add composition exercise.

--- composition/pipeline.js
+++ composition/pipeline.js
@@ -4,11 +4,22 @@ const { count, head, tail } = require('../arrayHelpers.js');
 const { reduce } = require('../higher-order/filterMapReduce.js');
 
 const pipeline = (...functions) => {
-  // TODO
+  if (count(functions) === 0) return (input) => input;
+  if (count(functions) === 1) return (input) => functions[0](input);
+
+  const recursion = (headFunction, tailFunctions) => {
+    return recursion((head(tailFunctions)(newInput)), tail(tailFunctions));
+  }
+
+  return (input) =>  pipeline(...tail(functions))(head(functions)(input));
 };
 
 const reducePipeline = (...functions) => {
-  // TODO
+  // reduce takes: 
+  // 1) reducer function,
+  // 2) initial value
+  // 3) array (of functions in this case)
+  return (input) => reduce((acc, fn) => fn(acc), input, functions);
 };
 
 
